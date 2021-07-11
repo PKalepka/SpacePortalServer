c1ce55d7d985ffad1e8350048cbfeea8ed3c992d
Pavlo Kalepka <p.kalepka@gmail.com>
2021-07-12 01:06:59 +0300

    Fixed CORS exception

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const express = require('express'),
-  config = require('config'),
+const express = require("express"),
+  config = require("config"),
   app = express(),
-  serviceConfig = config.get('Service'),
-  feedApi = require('./api/neoFeed');
+  serviceConfig = config.get("Service"),
+  feedApi = require("./api/neoFeed");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app
   //returns either online NASA API content or database cached result if it already exists
-  .route('/neo/feed')
+  .route("/neo/feed")
   .get((req, res) => {
     const startDate = req.query.start_date;
     const endDate = req.query.end_date;
 
-    return feedApi.neoFeed(startDate, endDate,
-      content => res.status(200).send(JSON.stringify(content)));
+    return feedApi.neoFeed(startDate, endDate, (content) =>
+      res
+        .setHeader("Access-Control-Allow-Origin", "*")
+        .status(200)
+        .send(JSON.stringify(content))
+    );
   });
 
 app.listen(serviceConfig.port, serviceConfig.host, () =>
-  console.log(`Server listens http://${serviceConfig.host}:${serviceConfig.port}`)
+  console.log(
+    `Server listens http://${serviceConfig.host}:${serviceConfig.port}`
+  )
 );
