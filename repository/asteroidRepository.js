8970d81875d017adbf1c055a33c5d3f9e1f54339
Pavlo Kalepka <p.kalepka@gmail.com>
2022-07-04 00:59:13 +0300

    fix: response when date range is not undefined

--- repository/asteroidRepository.js
+++ repository/asteroidRepository.js
@@ -14,6 +14,36 @@ module.exports = class AsteroidRepository {
       .getMany();
   };
 
+  getAsteroidsMinDate = async () => {
+    const connection = await this._getConnection();
+
+    return connection
+      .getRepository('asteroid')
+      .createQueryBuilder('asteroid')
+      .select("MIN(asteroid.date)", "value")
+      .getRawOne();
+  };
+
+  getAsteroidsMaxDate = async () => {
+    const connection = await this._getConnection();
+
+    return connection
+      .getRepository('asteroid')
+      .createQueryBuilder('asteroid')
+      .select("MAX(asteroid.date)", "value")
+      .getRawOne();
+  };
+
+  getAsteroidsCount = async () => {
+    const connection = await this._getConnection();
+
+    return connection
+      .getRepository('asteroid')
+      .createQueryBuilder('asteroid')
+      .select("COUNT(asteroid.id)", "Count")
+      .getRawOne();
+  };
+
   addAsteroids = async (content) => {
     const connection = await this._getConnection();
     const repository = connection.getRepository('asteroid');
--- services/feedService.js
+++ services/feedService.js
@@ -18,13 +18,20 @@ module.exports = class FeedApi {
     const tomorrow = new Date(today);
     tomorrow.setDate(tomorrow.getDate() + 1);
 
-    const startDate = typeof start !== 'undefined' ? start : this._getFormattedDate(today);
-    const endDate = typeof end !== 'undefined' ? end : this._getFormattedDate(tomorrow);
+    const startDate = typeof start !== 'undefined' ? new Date(Number(start)) : today;
+    const endDate = typeof end !== 'undefined' ? new Date(Number(end)) : tomorrow;
 
     return { startDate, endDate };
   }
 
-  async handleNewAsteroids(startDate, endDate) {
+  _getDateRangeFormatted(start, end) {
+    return {
+      startFormatted: this._getFormattedDate(start),
+      endFormatted: this._getFormattedDate(end),
+    };
+  }
+
+  async _handleNewAsteroids(startDate, endDate) {
     var newAsteroids = await this._externalService.getFeedOnline(startDate, endDate);
     const content = this._asteroidMapper.fromApi(newAsteroids);
     await this._asteroidRepository.addAsteroids(content);
@@ -32,10 +39,33 @@ module.exports = class FeedApi {
     return content;
   }
 
+  async _handleAsteroidsIfNotExists(start, end) {
+    if (start < end) {
+      const { startFormatted, endFormatted } = this._getDateRangeFormatted(start, end);
+      await this._handleNewAsteroids(startFormatted, endFormatted);
+    }
+  }
+
   async neoFeed(start, end) {
     const { startDate, endDate } = this._getDateRange(start, end);
-    const content = await this._asteroidRepository.listAsteroidsByDateRange(startDate, endDate);
+    const { startFormatted, endFormatted } = this._getDateRangeFormatted(startDate, endDate);
+    const minCachedDate = (await this._asteroidRepository.getAsteroidsMinDate()).value;
+    const maxCachedDate = (await this._asteroidRepository.getAsteroidsMaxDate()).value;
 
-    return content.length === 0 ? await this.handleNewAsteroids(startDate, endDate) : content;
+    // if cache is empty
+    // or
+    // if date range of request doesn't intersect with total date range of cached objects
+    const count = await this._asteroidRepository.getAsteroidsCount();
+    if (count === 0 || endDate < minCachedDate || maxCachedDate < startDate) {
+      return await this._handleNewAsteroids(startFormatted, endFormatted);
+    }
+
+    // otherwise add not existed objects into cache
+    await this._handleAsteroidsIfNotExists(startDate, minCachedDate);
+    await this._handleAsteroidsIfNotExists(maxCachedDate, endDate);
+
+    // ASSUMPTION: we`re assuming that cached date range wasn't interrupted and cache already contains all necessary objects inside cached date range
+    // If assumption is wrong than response will contain cahed objects only
+    return await this._asteroidRepository.listAsteroidsByDateRange(startFormatted, endFormatted);
   }
 };
